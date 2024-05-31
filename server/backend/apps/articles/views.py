7b3de09d9a1b108054dcd3b98559e2d42a0303f9
gragon <1526532001@qq.com>
2024-05-31 19:07:00 +0800

    后端修改

--- server/backend/apps/articles/views.py
+++ server/backend/apps/articles/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 from apps.articles.models import Article
 from apps.articles.serializes import ArticleSerializer
-from utils.smart_search import search
 from utils.utils import get, json_response, post, jwt_authentication
 
 
@@ -28,16 +27,4 @@ class ArticlesView():
     @get
     def search(request, *args, **kwargs):
         # 获取查询字符串参数
-        query_list = request.GET.getlist('query')
-        # 过滤掉空字符串
-        query_list = [q for q in query_list if q]
-
-        if not query_list:
-            return json_response(code=400,message= '查询参数不能为空')
-        # 组合所有查询字符串，假设使用 OR 操作符组合查询条件
-        query_str = " OR ".join(query_list)
-        articles = search(query_str)
-        articles_json = []
-        if len(articles) > 0:
-            articles_json = list(articles.values())
-        return json_response(data=articles_json,code=200,message='查询成功')
+        pass
--- server/backend/system/SystemController.py
+++ server/backend/system/SystemController.py
@@ -1,7 +1,5 @@
 # system/SystemController.py
 import os
-
-from utils.smart_search import create_search_index
 class SystemController:
     def __init__(self):
         pass  # 在初始化中不做任何事情
@@ -14,4 +12,3 @@ class SystemController:
         indexdir = "indexdir"
         if not os.path.exists(indexdir):
             os.makedirs(indexdir)
-        create_search_index()
